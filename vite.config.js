a2bab72a35f05961b3c59571411e6c31feb0f63e
Pedro Esteves <esteves7771@gmail.com>
2026-04-30 14:31:07 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/Zai/',
   plugins: [
     react(),
     VitePWA({
@@ -16,7 +17,7 @@ export default defineConfig({
         background_color: '#FAFAF7',
         display: 'standalone',
         orientation: 'portrait',
-        start_url: '/',
+        start_url: '/Zai/',
         icons: [
           { src: 'logo.png', sizes: '192x192', type: 'image/png' },
           { src: 'logo.png', sizes: '512x512', type: 'image/png' }
