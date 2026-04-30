ba38473e1bde49735f70835fa2e3a7335a13ece4
Pedro Esteves <esteves7771@gmail.com>
2026-04-30 16:44:54 +0100

    Update de.js

--- src/locales/de.js
+++ src/locales/de.js
@@ -76,6 +76,7 @@ export default {
     close: 'Schließen',
   },
   share: {
+    title: 'Share Zai',
     text: 'Entdecke Zai – scanne Produkte und wisse genau, was du isst! 🥭',
     copied: 'Link kopiert!',
   }
